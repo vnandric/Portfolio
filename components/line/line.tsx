0554a724172e4d9549be2a51a1ce5070628fed8b
ThaOniFans <valentinoandric200425@gmail.com>
2023-01-25 07:01:02 +0100

    lijntje er ook bij

---
+++ components/line/line.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/line.module.css";
+
+const line = () => {
+    return (
+        <div>
+            <div className={styles.line}></div>
+        </div>
+    )
+};
+
+export default line;
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -2,12 +2,12 @@ import styles from "../styles/navbar.module.css";
 
 const navbar = () => {
     return (
-        <div className={styles.navflex}>
+        <div className={styles.navFlex}>
             <nav className={styles.nav}>
                 <ul className={styles.ul}>
-                    <li>Hallo</li>
-                    <li>Bonjour</li>
-                    <li>hahaha</li>
+                    <li><a href="#">Hallo</a></li>
+                    <li><a href="#">Bonjour</a></li>
+                    <li><a href="#">hahaha</a></li>
                 </ul>
             </nav>
         </div>
