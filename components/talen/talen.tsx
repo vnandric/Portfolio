2cd173ff0071625772212fccdfb8a23ef7a35199
Valentino <valentinoandric200425@gmail.com>
2023-01-25 15:16:34 +0100

    talen erbij en beter structuur

--- components/talen/talen.tsx
+++ components/talen/talen.tsx
@@ -1,9 +1,14 @@
 import styles from "../styles/talen.module.css";
 
+import Image from "next/image";
+import html5pic from "../../src/media/htmlpic.png";
+
 const talen = () => {
     return (
-        <div id="talen" className={styles.background}>
-           
+        <div id="talen" className={styles.box}>
+            <div className={styles.htmlbox}>
+                <Image alt='jpg' src={html5pic} className={styles.html}></Image>
+            </div>
         </div>
     
 )};
