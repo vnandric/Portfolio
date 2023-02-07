878983568431d6baa7a0e364da0b76e92147ce09
Valentino <valentinoandric200425@gmail.com>
2023-02-07 12:31:28 +0100

    bismilllah

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ import Uv from "../../components/under-navbar/uv";
 
 import About from "../../components/about/about";
 import Projecten from "../../components/projecten/projecten";
-import Form from "../../components/form/Form";
 import { ScrollToTop } from "../../components/scrolltotop/ScrollToTop";
+import Form from "../../components/form/From";
 
 const Home: NextPage = () => {
   return (
